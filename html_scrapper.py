fcd306ae22a522dfb932720583ffaecf224ab246
akoss <akoss@origin>
2023-05-29 22:46:39 +0200

    First working version

---
+++ html_scrapper.py
@@ -0,0 +1,115 @@
+import requests
+import re
+import json
+from html.parser import HTMLParser
+from datetime import datetime
+from pprint import PrettyPrinter
+
+pprint = PrettyPrinter(indent=4, depth=4, sort_dicts=False)
+
+
+def get_url_html(url: str) -> str:
+    r = requests.get(url)
+    assert r.status_code == 200, f"Error: Request Status Code is '{r.status_code}'"
+    
+    return r.text
+
+
+def get_uniques(xs: list) -> list:
+    uniques = []
+    for x in xs:
+        if x not in uniques:
+            uniques.append(x)
+    return uniques
+
+
+es_months = ["enero",
+             "febrero",
+             "marzo",
+             "abril",
+             "mayo",
+             "junio",
+             "julio",
+             "agosto",
+             "septiembre",
+             "octubre",
+             "noviembre",
+             "diciembre"]
+
+
+def es_month_to_number(month: str) -> int:
+    return es_months.index(month) + 1
+
+    
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.pdf_url_prefix = 'https://www.aemet.es'
+        self.pdfs: dict = {}
+        self.last_pdf: str = ''
+        self.listen_for_date: bool = False
+        self.listen_for_name: bool = False
+    
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for attr in attrs:
+                if len(attr) > 1 and attr[0] == 'href' and attr[1].endswith('.pdf'):
+                    pdf_url = attr[1]
+                    self.pdfs.update({pdf_url: {
+                        'name': None,
+                        'date': None
+                    }})
+                    self.last_pdf = pdf_url
+                    self.listen_for_name = True
+                    self.listen_for_date = True
+
+    def handle_data(self, data):
+        if self.listen_for_name:
+            match = re.findall(r"\([0-9]{1,10} [MK]B\)", data)
+            if match:
+                pdf_name = data
+                self.pdfs[self.last_pdf]['name'] = pdf_name
+                self.listen_for_name = False
+        
+        if self.listen_for_date:
+            data_tokens: list = data.replace('.', ' ').split(' ')
+            for i, token in enumerate(data_tokens):
+                if token in es_months:
+                    day, month, year = data_tokens[i-2], token, data_tokens[i+2]
+                    try:
+                        possible_date = f"{year}/{es_month_to_number(month):02}/{day}"
+                        date = datetime.strptime(possible_date, "%Y/%m/%d")
+                    except ValueError:
+                        date = datetime(2000, 1, 1)
+                        # TODO: handle bad formatted dates in a better way, probably notifying users...
+                        print(f"\nWarning: Could not parse date '{possible_date}' - bad formatted, for pdf '{self.last_pdf}'. For now I will ignore it...\n")
+                    
+                    self.pdfs[self.last_pdf]['date'] = date.strftime("%Y/%m/%d")
+                    self.listen_for_date = False
+                    break
+
+    def __format_pdfs_found(self):
+        if len(self.pdfs) > 0:
+            pdfs = {}
+            for pdf_url, pdf_info in self.pdfs.items():
+                pdfs.update({pdf_info['name']: {
+                    'pdf_url': self.pdf_url_prefix + pdf_url,
+                    'pdf_date': pdf_info['date']
+                }})
+    
+            self.pdfs = pdfs
+
+    def get_pdfs(self) -> list:
+        self.__format_pdfs_found()
+        return self.pdfs
+                                 
+
+if __name__ == '__main__':
+    aemet_url = 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a1/acceso_libre/acceso_libre_2021_2022'
+    page = get_url_html(aemet_url)
+    parser = MyHTMLParser()
+    parser.feed(page)
+    pdfs = parser.get_pdfs()
+    print(pdfs)
+    print("Number of pdfs found:", len(pdfs))
+    
---
+++ main.py
@@ -0,0 +1,107 @@
+import os
+import logging
+import json
+from datetime import datetime
+
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, Job
+
+from html_scrapper import MyHTMLParser, get_url_html
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# ---------- config
+CHAT_ID = os.environ['CHAT_ID']
+BOT_TOKEN = os.environ['BOT_TOKEN']
+START_MSSG = 'Os mantendré informados!'
+TEMPLATE = './templates/template.txt'
+PDF_LISTS_PATH = './pdfs-registry'
+AEMET_URLS = {
+    # LABEL     : URL
+    'A1 Libre'  : 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a1/acceso_libre/acceso_libre_2021_2022',
+    'A2 Libre'  : 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a2/acceso_libre/acceso_libre_2021_2022',
+    'C1 Libre'  : 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_c1/acceso_libre/acceso_libre_2021_2022',
+    'A1 Interna': 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a1/promocion_interna/acceso_interna_2021_2022',
+    'A2 Interna': 'https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a2/promocion_interna/acceso_interna_2021_2022',
+}
+TIME_INTERVAL = 30    # in seconds
+# ----------
+
+
+def get_updated_pdfs_mssg(category: str, pdf_name: str, pdf_data: dict) -> str:
+    with open(TEMPLATE, 'r') as f:
+        mssg = f.read()
+        
+    mssg = mssg.replace('{pdf_name}', pdf_name)
+    mssg = mssg.replace('{category}', category)
+    mssg = mssg.replace('{pdf_url}', pdf_data['pdf_url'])
+    mssg = mssg.replace('{pdf_date}', pdf_data['pdf_date'])
+    return mssg
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=START_MSSG)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def scrap_coordinator(context: ContextTypes.DEFAULT_TYPE):
+    for category, url in AEMET_URLS.items():
+        await scrap_pdfs(context, category=category, url=url)
+    
+async def scrap_pdfs(context, category: str, url: str):
+    # get 'new' list of pdfs
+    page = get_url_html(url)
+    parser = MyHTMLParser()
+    parser.feed(page)
+    pdfs = parser.get_pdfs()
+
+    pdf_file_path = f"{PDF_LISTS_PATH}/pdfs-list-{category.replace(' ', '-')}.json"
+
+    # open 'old' list of pdfs
+    try:
+        with open(pdf_file_path, 'r') as f:
+            old_pdfs = json.load(f)
+    except:
+        print("No previous pdfs file found.")
+        old_pdfs = None
+
+    updated_pdfs = {}
+    if old_pdfs:
+        for pdf_name, pdf_data in pdfs.items():
+            if pdf_name in old_pdfs:
+                try:
+                    new_pdf_date = datetime.strptime(pdf_data['pdf_date'], '%Y/%m/%d')
+                    old_pdf_date = datetime.strptime(old_pdfs[pdf_name]['pdf_date'], '%Y/%m/%d')
+                except ValueError as exe:
+                    print("PDF: {pdf_name} for category {category} could not be processed")
+                if new_pdf_date > old_pdf_date:
+                    updated_pdfs.update({pdf_name: pdf_data})
+            else:
+                updated_pdfs.update({pdf_name: pdf_data})
+    else:
+        updated_pdfs = pdfs    
+
+    if len(updated_pdfs) > 0:
+        for pdf_name, pdf_data in updated_pdfs.items():
+            with open(pdf_file_path, 'w+') as f:
+                json.dump(pdfs, f)
+
+            mssg = get_updated_pdfs_mssg(category, pdf_name, pdf_data)
+            await context.bot.send_message(chat_id=CHAT_ID, text=mssg, parse_mode='HTML')
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    job_queue = application.job_queue
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    job_scrap = job_scrap_pdfs = job_queue.run_repeating(scrap_coordinator, interval=TIME_INTERVAL, first=1)
+    
+    application.run_polling()
+    
