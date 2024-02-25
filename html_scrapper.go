8f8e2d38e7407f5f6c26f6261115ac3134f8c22a
Alberto Calzada <albertocalzada95@gmail.com>
2024-02-25 20:01:29 +0100

    Set up bot

--- html_scrapper.go
+++ html_scrapper.go
@@ -9,19 +9,42 @@ import (
 	"strconv"
 	"regexp"
 	"time"
+	"os"
 	"net/http"
 	"golang.org/x/net/html"
-	// tele "gopkg.in/telebot.v3"
+	tele "gopkg.in/telebot.v3"
 )
 
+// ----------
+type BotConfig struct {
+	TimeInterval time.Duration
+	ChatIds      map[string]string
+	StartMessage string
+	TemplatePath string
+	RegistryPath string
+	AemetUrls    map[string]map[string]string
+}
+
 var (
+	bot_config = BotConfig{
+		TimeInterval: 5 * time.Second,    // in seconds
+		ChatIds: map[string]string{
+			"TEST": os.Getenv("TG_AEMET_TOKEN"),
+		},
+		StartMessage: "Os mantendré informados!",
+		TemplatePath: "./templates/template_fmt.txt",
+		RegistryPath: "./pdfs-registry",
+		AemetUrls: map[string]map[string]string{
+			"TEST": {
+				"Libre"  : "https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a1/acceso_libre/acceso_libre_2021_2022",
+			},
+		},
+	}
 	MONTHS_ES = map[string]time.Month{"enero": time.January, "febrero": time.February, "marzo": time.March, "abril": time.April, "mayo": time.May, "junio": time.June, "julio": time.July, "agosto": time.August, "septiembre": time.September, "octubre": time.October, "noviembre": time.November, "diciembre": time.December}
 	DEFAULT_DATE = time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
 )
 
 const(
-	url = "https://www.aemet.es/es/empleo_y_becas/empleo_publico/oposiciones/grupo_a1/acceso_libre/acceso_libre_2021_2022"
-	PARSING_FINISHED = "PARSING_FINISHED"
 	DATE_REGEXP = `[0-9]{1,2} de[l]{0,1} [a-z]{1,10} de[l]{0,1} [0-9]{3,4}|[0-9]{1,2} de [a-z]{1,10}`
 	DATE_LAYOUT = "02/01/2006"
 )
@@ -134,35 +157,88 @@ func gen_pdfs(r io.Reader, pdfs chan PDF) {
 	return
 }
 
-func main() {
-	var client *http.Client = &http.Client{}
-	res, err := client.Get(url)
-	if err != nil && res.StatusCode == 200 {
-		log.Fatalf("Something went wrong getting url. StatusCode: %d", res.StatusCode)
-	}
-
-	ch := make(chan PDF)
-	go gen_pdfs(res.Body, ch)
-
-	// start pooling for pdfs
-	pdfs := make([]PDF, 50, 100)
-	count := 0
-	for pdf, ok := <-ch; ok; pdf, ok = <-ch {
-		if count > cap(pdfs) {
-			res.Body.Close()
-			log.Fatalf("Too many pdfs found. Increase buffer capacity")
-
+func read_complete_file(path string) string {
+	if f, err := os.Open(bot_config.TemplatePath); err != nil {
+		log.Fatalf("Could not open template file: %s", err)
+		return ""
+	} else {
+		if t, err := io.ReadAll(f); err != nil {
+			log.Fatalf("Could not read teamplate file: %s", err)
+			return ""
+		} else {
+			return string(t)
 		}
-		pdfs[count] = pdf
-		count++
 	}
-	res.Body.Close()
+}
 
+var template = read_complete_file(bot_config.TemplatePath)
 
-	for i, pdf := range pdfs {
-		if len(pdf.name) == 0 { break }
-		fmt.Printf("(%d) new pdf:\n\tname: %s\n\turl: %s\n\tdate: %s\n\n", i, pdf.name, pdf.url, pdf.date)
+type ProcessingError int
+const (
+	UpdateError ProcessingError = iota
+)
+
+func process_updates(ctxs []tele.Context, err_ch chan ProcessingError) {
+	for _, c := range ctxs {
+		proc := func () {
+			if c != nil {
+				var client *http.Client = &http.Client{}
+				res, err := client.Get(bot_config.AemetUrls["TEST"]["Libre"])
+				if err != nil && res.StatusCode == 200 {
+					log.Fatalf("Something went wrong getting url. StatusCode: %d", res.StatusCode)
+				}
+
+				pdfs := make(chan PDF)
+				go gen_pdfs(res.Body, pdfs)
+				for pdf, ok := <-pdfs; ok; pdf, ok = <-pdfs {
+					message := fmt.Sprintf(template,
+						"TEST",
+						"https://www.aemet.es",
+						pdf.url,
+						pdf.name,
+						pdf.date,
+					)
+					if err := c.Send(message, &tele.SendOptions{ParseMode: "HTML"}); err != nil {
+						err_ch <- UpdateError
+					}
+					res.Body.Close()
+				}
+			}
+		}
+		go proc()
+	}
+	return
+}
+
+func main() {
+	sett := tele.Settings{
+		Token: bot_config.ChatIds["TEST"],
+		Poller: &tele.LongPoller{Timeout: bot_config.TimeInterval},
 	}
 
-	log.Println("Finished successfully!")
+	bot, err := tele.NewBot(sett)
+	if err != nil {
+		log.Fatalf("Could not instantiate bot: %s", err)
+		return
+	}
+
+	ctxs := make([]tele.Context, 20)
+	bot.Handle("/start", func (c tele.Context) error {
+		ctxs = append(ctxs, c)
+		return c.Send(bot_config.StartMessage)
+	})
+
+	go bot.Start()
+
+	err_chan := make(chan ProcessingError, 50)
+	for {
+		go process_updates(ctxs, err_chan)
+		select {
+		case <-err_chan:
+			log.Printf("Error when processing updates, %s", err)
+		default:
+			log.Println("All good!")
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
