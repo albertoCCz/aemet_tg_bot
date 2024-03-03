0322e544a02daa5058e27565975d30e0fd6e9648
Alberto Calzada <albertocalzada95@gmail.com>
2024-03-03 10:16:30 +0100

    Implement command line arguments for bot

--- bot.go
+++ bot.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 	"os"
+	"strings"
 	"errors"
 	"net/http"
 	"encoding/json"
@@ -60,7 +61,7 @@ func (errMessage *processingErrorMessage) Format() string {
 
 type pdfRegistry map[string]map[string]string
 
-func processUpdates(bot *tele.Bot, botConfig *BotConfig, err_ch chan processingErrorMessage) {
+func processUpdates(bot *tele.Bot, botConfig *BotConfig, err_ch chan processingErrorMessage, send_on bool) {
 	var procChat func(ChatConfig)
 	procChat = func(c ChatConfig) {
 		for _, sp := range c.SelectiveProcs {
@@ -93,19 +94,15 @@ func processUpdates(bot *tele.Bot, botConfig *BotConfig, err_ch chan processingE
 				return
 			}
 
-			parse_registry := true
 			registry_data, err := os.ReadFile(sp.RegistryPath)
+			var registry = pdfRegistry{}
 			if err != nil {
 				log.Printf("[WARNING] Chat '%s' - Selective process '%s'. Could not read registry from path '%s'\n", c.Name, sp.Name, sp.RegistryPath)
 				err_message.errCode = ReadRegistryError
 				err_message.message = err
 				err_ch <- err_message
-				parse_registry = false
 				// file will be created later, so we dont return in this case
-			}
-
-			var registry = pdfRegistry{}
-			if parse_registry {
+			} else {
 				err = json.Unmarshal(registry_data, &registry)
 				if err != nil {
 					log.Printf("[ERROR] Chat '%s' - Selective process '%s'. Could not parse JSON from registry data\n", c.Name, sp.Name)
@@ -152,20 +149,22 @@ func processUpdates(bot *tele.Bot, botConfig *BotConfig, err_ch chan processingE
 						continue
 					}
 
-					message := fmt.Sprintf(string(template),
-						sp.Name,
-						"https://www.aemet.es",
-						pdf.Url,
-						pdf.Name,
-						pdf.Date,
-					)
-					if _, err := bot.Send(&c, message, &tele.SendOptions{ParseMode: "HTML"}); err != nil {
-						log.Printf("[ERROR] Chat '%s' - Selective process '%s'. Could not send message to chat%s\n", c.Name, sp.Name, err)
-						err_message.errCode = SendMessageError
-						err_message.pdfName = pdf.Name
-						err_message.message = err
-						err_ch <- err_message
-						continue
+					if send_on {
+						message := fmt.Sprintf(string(template),
+							sp.Name,
+							"https://www.aemet.es",
+							pdf.Url,
+							pdf.Name,
+							pdf.Date,
+						)
+						if _, err := bot.Send(&c, message, &tele.SendOptions{ParseMode: "HTML"}); err != nil {
+							log.Printf("[ERROR] Chat '%s' - Selective process '%s'. Could not send message to chat%s\n", c.Name, sp.Name, err)
+							err_message.errCode = SendMessageError
+							err_message.pdfName = pdf.Name
+							err_message.message = err
+							err_ch <- err_message
+							continue
+						}
 					}
 				} // if pdf !exists
 			} // each pdf
@@ -179,9 +178,9 @@ func processUpdates(bot *tele.Bot, botConfig *BotConfig, err_ch chan processingE
 	return
 }
 
-func main() {
+func handle_run_command(configPath string) {
 	var botConfig BotConfig
-	botConfig.SetUp("./botConfig.json")
+	botConfig.SetUp(configPath)
 
 	sett := tele.Settings{
 		Token: botConfig.Token,
@@ -208,8 +207,113 @@ func main() {
 			}
 		default:
 			log.Println("[INFO] New round!")
-			go processUpdates(bot, &botConfig, err_chan)
+			go processUpdates(bot, &botConfig, err_chan, true)
 			time.Sleep(botConfig.TimeInterval)
 		}
+	}
+}
+
+func handle_init_command(configPath string) {
+	var botConfig BotConfig
+	botConfig.SetUp(configPath)
+
+	sett := tele.Settings{
+		Token: botConfig.Token,
+		Poller: &tele.LongPoller{Timeout: botConfig.TimeInterval},
+	}
+
+	bot, err := tele.NewBot(sett)
+	if err != nil {
+		log.Fatalf("Could not instantiate bot: %s\n", err)
+		return
+	}
+
+	go bot.Start()
+
+	log.Println("[INFO] Starting initialisation.")
+	err_chan := make(chan processingErrorMessage, 50)
+	all_processed := false
+	for {
+		select {
+		case errMessageData := <-err_chan:
+			if botConfig.ChatAdminConfig != nil {
+				errMessage := errMessageData.Format()
+				if _, err := bot.Send(botConfig.ChatAdminConfig, errMessage, &tele.SendOptions{ParseMode: "HTML"}); err != nil {
+					log.Printf("[ERROR] Could not send error message to admin chat: %s\n", err)
+				}
+			}
+			fmt.Println("error case")
+		default:
+			if !all_processed {
+				go processUpdates(bot, &botConfig, err_chan, false)
+				all_processed = true
+				time.Sleep(botConfig.TimeInterval * 2)
+			}
+			log.Println("[INFO] Registries initialised!")
+			return
+		}
+	}
+}
+
+func usage() {
+	fmt.Println(
+		"usage: bot <command> [--bot-config=<config-path>]\n\n" +
+		"commands:\n" +
+	    "    help       Print this help.\n" +
+		"    run        Start running the bot. It needs the --bot-config flag.\n" +
+		"    init       Initialise the registries by running the bot. It needs the --bot-config flag.\n" +
+		"               Only the error messages to admin chat (if configured) will be sent.\n")
+}
+
+func nextFlagValue(command, flag string, args []string) string {
+	if !strings.HasPrefix(args[0], flag) {
+		usage()
+		fmt.Printf("[ERROR] Unknown flag '%s' for command '%s'\n", args[0], command)
+		os.Exit(-1)
+	}
+
+	flag_split := strings.SplitN(args[0], "=", 2)
+	if len(flag_split) < 2 {
+		usage()
+		fmt.Printf("[ERROR] Missing value for flag '%s' in '%s' command\n", flag, command)
+		os.Exit(-1)
+	}
+
+	return flag_split[1]
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		usage()
+		fmt.Println("[ERROR] No command provided")
+		os.Exit(-1)
+	}
+
+	switch command := os.Args[1]; command {
+	case "help":
+		usage()
+		return
+	case "run":
+		flag := "--bot-config"
+		if len(os.Args) <= 2 {
+			usage()
+			fmt.Printf("[ERROR] You need to pass the flag '%s' with command '%s'\n", flag, command)
+			os.Exit(-1)
+		}
+		configPath := nextFlagValue(command, flag, os.Args[2:])
+		handle_run_command(configPath)
+	case "init":
+		flag := "--bot-config"
+		if len(os.Args) <= 2 {
+			usage()
+			fmt.Printf("[ERROR] You need to pass the flag '%s' with command '%s'\n", flag, command)
+			os.Exit(-1)
+		}
+		configPath := nextFlagValue(command, flag, os.Args[2:])
+		handle_init_command(configPath)
+	default:
+		usage()
+		fmt.Printf("[ERROR] Unknown command '%s'\n", command)
+		os.Exit(-1)
 	}
 }
